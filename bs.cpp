f02fe23043f39208ee1d5c4e3afb250f5326006d
Tejasshinde1214 <121288341+Tejasshinde1214@users.noreply.github.com>
2023-05-25 22:04:13 +0530

    Create bs.cpp

---
+++ bs.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"Enter the size of array =>";
+    cin>>n;
+   
+    int arr[n];
+    cout<<"Enter "<<n<<" Integers in ascending order => \n";
+    for(int i=0;i<n;i++)
+        cin>>arr[i];    
+   
+    int target;
+    cout<<"Enter the number you want to search => ";
+    cin>>target;
+   
+    int start = 0, end = n-1, mid;
+    int pos= -1;
+   
+    while(start<=end)
+    {
+        mid = (start+end)/2;
+       
+        if(arr[mid]==target){
+            pos = mid;
+            break;
+        }
+        else if(arr[mid]<target)
+            end = mid -1;  
+        else
+            start = mid + 1;
+    }
